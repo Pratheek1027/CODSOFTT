0341e95b070826cbb4e20638a65a06f17d167f58
Pratheek Shetty <spratheek113@gmail.com>
2025-12-09 12:32:23 +0530

    use files

---
+++ titanic/titan.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+df = pd.read_csv("titanic.csv")
+print(df.head())
+print(df.info())
+print(df.isnull().sum())
+df['Age'].fillna(df['Age'].median(), inplace=True)
+df['Embarked'].fillna(df['Embarked'].mode()[0], inplace=True)
+df.drop('Cabin', axis=1, inplace=True)
+df['Sex'] = df['Sex'].map({'male': 0, 'female': 1})
+df['Embarked'] = df['Embarked'].map({'S': 0, 'C': 1, 'Q': 2})
+X = df[['Pclass', 'Sex', 'Age', 'Fare']]
+y = df['Survived']
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+model = LogisticRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+cm = confusion_matrix(y_test, y_pred)
+sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+plt.show()
+from sklearn.metrics import classification_report
+print(classification_report(y_test, y_pred))
+new_passenger = [[3, 0, 22, 7.25]]
+prediction = model.predict(new_passenger)
+print("Survived?" , prediction)
+
+
