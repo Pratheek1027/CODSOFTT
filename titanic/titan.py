8a3a16202aa5d782a255d1f1826b1226f38f111a
Pratheek Shetty <spratheek113@gmail.com>
2025-12-10 20:58:37 +0530

    random

--- titanic/titan.py
+++ titanic/titan.py
@@ -3,7 +3,8 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 df = pd.read_csv("titanic.csv")
 print(df.head())
@@ -19,7 +20,10 @@ y = df['Survived']
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
-model = LogisticRegression()
+model = RandomForestClassifier(
+    n_estimators=100,
+    random_state=42
+)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
