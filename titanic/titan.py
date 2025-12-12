6fa13412a654b35b529b7c2ccb8a6ed77b6d64a3
Pratheek Shetty <spratheek113@gmail.com>
2025-12-12 23:34:12 +0530

    accuracyin

--- titanic/titan.py
+++ titanic/titan.py
@@ -19,8 +19,25 @@ y = df['Survived']
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
-model = LogisticRegression()
-model.fit(X_train, y_train)
+from sklearn.preprocessing import StandardScaler
+
+# Scale the features
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Improved Logistic Regression
+model = LogisticRegression(
+    solver='liblinear',
+    max_iter=2000,
+    class_weight='balanced'
+)
+
+model.fit(X_train_scaled, y_train)
+
+# Predict
+y_pred = model.predict(X_test_scaled)
+
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)
@@ -35,9 +52,15 @@ plt.ylabel("Actual")
 plt.show()
 from sklearn.metrics import classification_report
 print(classification_report(y_test, y_pred))
+# Predict for new passenger
 new_passenger = [[3, 0, 22, 7.25]]
-prediction = model.predict(new_passenger)
-print("Survived?" , prediction)
+prediction = model.predict(new_passenger)[0]  # take first value
+
+# Convert numeric result into text
+if prediction == 1:
+    print("Prediction: This passenger would have SURVIVED 🚢")
+else:
+    print("Prediction: This passenger would NOT have survived ❌")
 
 
 
